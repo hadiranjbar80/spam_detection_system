2831e58eba85c4b345127ee7552a0d092afd2194
Mohammad Javad <mrakhshani88@gmail.com>
2023-05-24 15:52:07 +0330

    Commit 24 May (merge)

--- parser_test.py
+++
@@ -1,6 +0,0 @@
-import parser.EmailParser
-
-parser = parser.EmailParser.EmailParser("sample.txt")
-print("[From]:", parser.parse("from"))
-print("[Subject]:", parser.parse("subject"))
-print("[Content]:", parser.parse("content"))
